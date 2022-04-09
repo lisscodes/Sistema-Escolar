b25cfc3b509378aacc047a458c54dbe3e9eccbe6
mrjulia22 <maria.jaraujo@ufpe.br>
2022-04-09 09:31:15 -0300

    adicionando arquivo Alune.java

---
+++ src/sistema_escolar/Alune.java
@@ -0,0 +1,41 @@
+package entities;
+
+public class Alune extends Pessoa {
+	
+	private Curso curso;
+	
+	public Alune (String nome, int idade, String cpf, Curso curso) {
+		super (nome, idade, cpf);
+		this.curso = curso;
+		
+	}
+	
+	
+	@Override
+	public String toString() {
+		return "Alune"+
+				super.toString()+
+				"Código do curso: " + curso.getMatricula()+
+				"Nome do curso: "+ curso.getNomeCurso();
+		
+	}
+
+
+	public Curso getCurso() {
+		return curso;
+	}
+
+
+	public void setCurso(Curso curso) {
+		this.curso = curso;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+
+}
